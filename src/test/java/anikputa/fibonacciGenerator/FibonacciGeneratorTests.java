d6fb967de270e4d0decb1f22b79547be60bd572b
deepmud <73426734+deepmud@users.noreply.github.com>
2021-04-25 20:51:45 +0100

    THANKS

--- src/test/java/anikputa/fibonacciGenerator/FibonacciGeneratorTests.java
+++ src/test/java/anikputa/fibonacciGenerator/FibonacciGeneratorTests.java
@@ -4,46 +4,52 @@ package anikputa.fibonacciGenerator;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import javax.annotation.Nullable;
+
 public class FibonacciGeneratorTests {
 
     @Test
     void shouldGenerateIndexNumbersOneToTen(){
         FibonacciGenerator fibonacciGenerator = new FibonacciGenerator();
-        //int[] s = {0,1,1,2,3,5,8,13};
-
-        Assertions.assertArrayEquals(generatenumber1To100(),fibonacciGenerator.GenerateIndexNumbersOneToTen());
-
-
+        int[] expected = {0,1,1,2,3,5,8,13,21,34};
+        Assertions.assertArrayEquals(expected,fibonacciGenerator.
+                GenerateIndexNumbersOneToTen());
     }
 
-    public int[] generatenumber1To100() {
-        int[] numbers = new int[10];
+    @Test
+    void GenerateUserFibonacciIndexNumber(){
+        FibonacciGenerator fibonacciGenerator = new FibonacciGenerator();
+        Assertions.assertArrayEquals(GenerateIndexNumbersOneToTen(),
+                fibonacciGenerator.GenerateUserFibonacciIndexNumber(10));
+    }
 
-        numbers[0]=0;
-        numbers[1]=1;
-        for(int i = 2; i < 10; i++){
-            numbers[i] = numbers[i-2] + numbers[i-1];
+    public int[] GenerateIndexNumbersOneToTen(){
+
+            int[] numbers = new int[10];
+
+            numbers[0] = 0;
+            numbers[1] = 1;
+            for (int i = 2; i < 10; i++) {
+                numbers[i] = numbers[i - 2] + numbers[i - 1];
+            }
+            return numbers;
+    }
+
+    public int[] GenerateUserFibonacciIndexNumber(int userFibonacciIndex) {
+        int[] numbers = new int[userFibonacciIndex];
+
+        System.out.print("FINOBACCI SERIES:  ");
+        numbers[0] = 0;
+        System.out.print(numbers[0] + "  ");
+        numbers[1] = 1;
+        System.out.print(numbers[1] + "  ");
+        for (int i = 2; i < userFibonacciIndex; i++) {
+            numbers[i] = numbers[i - 2] + numbers[i - 1];
         }
         return numbers;
     }
 
+
+
 }
 
-/* fibonacci index of 8
-
-    0 1 1 2 3 5 8 13 21
-    0 1 2 3 4 5 6 7 8
-
-    if index of 8 is checked;
-
-    do;
-        0+1+1=2 is index of 2
-        0+1+1=2 is index of 3
-        1+2=3 is index of 4
-        2+3=5 is index of 5
-        3+5=8 is index of 6
-        5+8=13 is index of 7
-
-
-
- */
